fae97e7bc17e9ee3314e0a810f6dded5b29349d7
Amrit Raj <amrit781@gmail.com>
2021-02-27 18:56:54 +0530

    Intial commit with working CRUD operations

---
+++ src/main/java/com/crud/tutorial/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.crud.tutorial.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crud.tutorial.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, Long> {
+
+}
